8fd7941409ce1abcb65294211ffa8777d1236493
marr75 <matt@mysidewalk.com>
2023-03-17 18:26:20 -0500

    Updated to fix the issue with slaves becoming colonists

--- Source/Main.cs
+++ Source/Main.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
 using JetBrains.Annotations;
+using RimWorld;
+using RimWorld.Planet;
 using Verse;
 
 namespace PrisonerCaravanFix; 
@@ -16,16 +20,74 @@ class Main {
     }
 }
 
+#if DEBUG 
+[HarmonyDebug]
+#endif
 [HarmonyPatch(typeof(Pawn), nameof(Pawn.ExitMap))]
 static class PawnExitMapFix {
 
-    [UsedImplicitly]
-    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
-        var myInstructions = instructions.ToArray();
-        var isPrisonerMethod = AccessTools.PropertyGetter(typeof(Pawn), nameof(Pawn.IsPrisoner));
-        var callsPrisonerIndex = myInstructions.FirstIndexOf(i => i.Calls(isPrisonerMethod));
-        var replaceIndex = callsPrisonerIndex + 1;
-        myInstructions[replaceIndex].opcode = OpCodes.Brtrue_S;
-        return myInstructions.AsEnumerable();
+    [Conditional("DEBUG")]
+    static void DebugLog(string message) {
+        Log.Message(message);
+    }
+    
+    static bool IsFree(Pawn target) {
+        var isInDropPod = ThingOwnerUtility.AnyParentIs<ActiveDropPodInfo>(target)
+            || ThingOwnerUtility.AnyParentIs<TravelingTransportPods>(target);
+        var result = (!target.IsCaravanMember() && !target.teleporting && !isInDropPod)
+            || (!target.IsPrisoner && !target.IsSlaveOfColony && !isInDropPod)
+            || target.guest is { Released: true };
+        return result;
+    }
+    
+    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator) {
+        var instructionArray = instructions.ToArray();
+        DebugLog(
+            $"[WSK PrisonerCaravanFix] Transpiling Pawn.ExitMap method. Method has {instructionArray.Length} IL instructions."
+        );
+
+        // Instruction to call our IsFree method
+        var loadThisInstruction = new CodeInstruction(OpCodes.Ldarg_0);
+        var callIsFreeInstruction = new CodeInstruction(
+            OpCodes.Call,
+            AccessTools.DeclaredMethod(
+                typeof(PawnExitMapFix),
+                nameof(IsFree)
+            )
+        );
+        var storeFreeInstruction = new CodeInstruction(OpCodes.Stloc_2);
+        var myInstructions = new[] {
+            loadThisInstruction, // `this`
+            callIsFreeInstruction, // will call `IsFree(this)` from the Exitmap method
+            storeFreeInstruction // will store the result of the call in Stloc_2, which is the `free` variable
+        };
+        
+        // `bool flag` is stored at Stloc_1, 7 instructions earlier is the start of the conditional to assign flag
+        // This is the start of the code to replace
+        var startIndex = instructionArray.FirstIndexOf(ci => ci.opcode == OpCodes.Stloc_1) - 7;
+        // Quite sensibly, the code to replace ends when the method calls Ldloc_2 (as we are storing in this location)
+        var endIndex = instructionArray.FirstIndexOf(ci => ci.opcode == OpCodes.Ldloc_2);
+        // We will replace the instructions from startIndex to endIndex with this call to our IsFree method instead and load the result
+
+        DebugLog(
+            $"[WSK PrisonerCaravanFix] startIndex is {startIndex}, endIndex is {endIndex}."
+        );
+        var instructionsBefore = instructionArray.Take(startIndex).ToArray();
+        DebugLog(
+            $"[WSK PrisonerCaravanFix] Last instruction of instructionsBefore is {instructionsBefore.Last().opcode} - {instructionsBefore.Last().operand}"
+        );
+        var instructionsAfter = instructionArray.Skip(endIndex).ToArray();
+        DebugLog(
+            $"[WSK PrisonerCaravanFix] First instruction of instructionsAfter is {instructionsAfter.First().opcode} - {instructionsAfter.First().operand}"
+        );
+        var skippedInstructions = instructionArray.Skip(startIndex).TakeWhile(ci => ci.opcode != OpCodes.Ldloc_2);
+        var labelsToFix = skippedInstructions.SelectMany(ci => ci.labels);
+        loadThisInstruction.labels.AddRange(labelsToFix);
+        var result = instructionsBefore.Concat(myInstructions).Concat(instructionsAfter).ToArray();
+        DebugLog(
+            $"[WSK PrisonerCaravanFix] Finished preparing new instructions for Pawn.ExitMap. Method now has {result.Length} IL instructions."
+        );
+        
+        return result;
     }
 }
